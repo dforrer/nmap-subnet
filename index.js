0f98998aa1459d7221044055c56c57db3077e7d3
dforrer <dforrer@gmail.com>
2019-08-16 13:01:11 +0200

    added total number of clients result output

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ nmap.nmapLocation = 'nmap'; //default
 var quickscan = new nmap.NmapScan('-sP 192.168.0.0/24');
 
 quickscan.on('complete', function (data){
-    //console.log(data);
-  
+    console.log("Clients found: " + data.length);
+
     for ( i = 0; i < data.length; i++ ) {
         var line   = data[i]['ip'] + ";";
 
@@ -23,9 +23,9 @@ quickscan.on('complete', function (data){
 
         console.log( line );
     }
-  
+
 });
- 
+
 quickscan.on('error', function(error){
   console.log(error);
 });
